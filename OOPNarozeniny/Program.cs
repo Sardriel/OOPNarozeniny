177cbfb2a5d24480a8d73ac5a224f14c9a9f6aab
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-14 11:48:27 +0100

    Add project files.

---
+++ OOPNarozeniny/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOPNarozeniny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej datum narození: ");
+            DateTime datumNarozeni = DateTime.Parse(Console.ReadLine());
+            TimeSpan vek = DateTime.Today - datumNarozeni;
+            DateTime dalsiNarozeniny = new DateTime(DateTime.Today.AddYears(1).Year, datumNarozeni.Month, datumNarozeni.Day);
+            TimeSpan index = dalsiNarozeniny - DateTime.Today;
+           
+            Console.WriteLine("Je ti {0} let a narozeniny máš za {1} dní.", Math.Floor(vek.Days/365.255), index.TotalDays);
+            Console.ReadKey();
+        }
+    }
+}
