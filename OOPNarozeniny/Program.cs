049c9064208ba033d9742d50501a87255f20cf24
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-15 09:47:21 +0100

    Absolutní hodnota

--- OOPNarozeniny/Program.cs
+++ OOPNarozeniny/Program.cs
@@ -9,10 +9,10 @@ namespace OOPNarozeniny
             Console.Write("Zadej datum narození: ");
             DateTime datumNarozeni = DateTime.Parse(Console.ReadLine());
             TimeSpan vek = DateTime.Today - datumNarozeni;
-            DateTime dalsiNarozeniny = new DateTime(DateTime.Today.AddYears(1).Year, datumNarozeni.Month, datumNarozeni.Day);
+            DateTime dalsiNarozeniny = new DateTime(DateTime.Today.Year, datumNarozeni.Month, datumNarozeni.Day);
             TimeSpan index = dalsiNarozeniny - DateTime.Today;
            
-            Console.WriteLine("Je ti {0} let a narozeniny máš za {1} dní.", Math.Floor(vek.Days/365.255), index.TotalDays);
+            Console.WriteLine("Je ti {0} let a narozeniny máš za {1} dní.", Math.Floor(vek.Days/365.255), Math.Abs(index.TotalDays));
             Console.ReadKey();
         }
     }
